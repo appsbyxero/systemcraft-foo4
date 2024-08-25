ceb72d66b41e2815d97029b767688873aef3342a
appsbyxero <appxero@gmail.com>
2024-08-25 12:08:30 +0530

    Provide starter code

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 import sys
 
+# This passes the first task!
 def main():
-    # Put your code here!
-    pass
+    with open(sys.argv[2], 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            print(line, end='')
+
 
 if __name__ == '__main__':
     main()
